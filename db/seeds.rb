afc9a6822f8985cb4243dc8fc677e44f44f3f4cf
Michael Wautier <wautiermichael@gmail.com>
2020-11-04 11:13:20 +0100

    added seed, controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Creating 5 restaurants...'
+
+Restaurant.create(name: 'Orient Express', address: '1400 Nivelles', category: 'chinese')
+Restaurant.create(name: 'La Villa Emily', address: '1000 Brussels', category: 'french')
+Restaurant.create(name: 'Poke\'Bowl', address: '1348 Louvain-La-Neuve', category: 'japanese')
+Restaurant.create(name: 'Arlechino', address: '7100 La Louvière', category: 'italian')
+Restaurant.create(name: 'Friterie Le 119', address: '14 Nivelles', category: 'belgian')
+
+puts 'Finished !'
